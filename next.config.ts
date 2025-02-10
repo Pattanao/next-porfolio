5e02647f3fadf491ac1e1db26419a8d773ffd40e
Pattanao <pat.sutanc@gmail.com>
2025-02-10 21:48:49 +0700

    setup project

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  compilerOptions: {
+    baseUrl: ".",
+    paths: {
+      "@/components/*": ["src/components/*"],
+      "@/styles/*": ["src/styles/*"],
+    },
+  },
 };
 
 export default nextConfig;
